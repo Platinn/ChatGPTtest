ff6fc141f1cf6c60ab312b9cf6f53814fd431fad
Pierre-LouisBJT <plbiojout@gmail.com>
2023-03-30 15:41:45 -0700

    Added the memory of previous chats

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { sendMessage } from './ChatGPTAPI';
+import { sendMessage, sendMessageWithContext } from './ChatGPTAPI';
 import styles from './Chat.module2.css';
 
 // Get the token from session storage or prompt the user to enter it
@@ -14,16 +14,30 @@ if (!userToken) {
 const App = () => {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
+  const [useContext, setUseContext] = useState(true); // useContext is now a state variable
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (input.trim() === '') return;
 
-    setMessages([...messages, { type: 'user', text: input }]);
+    //setMessages([...messages, { role: 'user', content: input }]);
+    //setInput('');
+
+    const newMessage = { role: 'user', content: input };
+    const updatedMessages = [...messages, newMessage];
+    setMessages(updatedMessages);
     setInput('');
 
-    const response = await sendMessage(input, userToken);
-    setMessages((prevMessages) => [...prevMessages, { type: 'gpt', text: response }]);
+    if (useContext) {
+
+      const response = await sendMessageWithContext(updatedMessages, userToken);
+      setMessages([...updatedMessages, { role: 'assistant', content: response }]);
+
+    } else {
+      //no memory of previous chats
+      const response = await sendMessage(input, userToken);
+      setMessages((prevMessages) => [...prevMessages, { role: 'assistant', content: response }]);
+    }
   };
 
   return (
@@ -31,8 +45,8 @@ const App = () => {
       <div className={styles.chatWindow}>
         {messages.map((message, index) => (
           <div key={index} className={styles.messageContainer}>
-            <div className={message.type === 'user' ? styles.userMessage : styles.gptMessage}>
-              {message.text}
+            <div className={message.role === 'user' ? styles.userMessage : styles.gptMessage}>
+              {message.content}
             </div>
           </div>
         ))}
