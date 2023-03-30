8c12256c22e1f70155700d99cda94c05c8807f17
Carracalla <Catastroph1c76@gmail.com>
2023-03-30 16:58:40 -0700

    Darkmode V1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import { sendMessage, sendMessageWithContext } from './ChatGPTAPI';
 import TokenInput from './TokenInput';
-import styles from './Chat.module2.css';
+import styles from './Chat.module.css';
+import darkStyles from './ChatDark.module.css';
 
 const App = () => {
   const [userToken, setUserToken] = useState(sessionStorage.getItem('userToken'));
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
   const [useContext, setUseContext] = useState(true); // useContext is now a state variable
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -39,22 +46,36 @@ const App = () => {
   }
 
   return (
-    <div className={styles.chatContainer}>
-      <div className={styles.chatWindow}>
+    <div className={`${styles.chatContainer} ${darkMode ? darkStyles.chatContainer : ''}`}>
+      <div className={styles.darkModeToggle}>
+        <label>
+          Dark mode
+          <input type="checkbox" onChange={toggleDarkMode} />
+        </label>
+      </div>
+      <div className={`${styles.chatWindow} ${darkMode ? darkStyles.chatWindow : ''}`}>
         {messages.map((message, index) => (
           <div key={index} className={styles.messageContainer}>
-            <div className={message.role === 'user' ? styles.userMessage : styles.gptMessage}>
+            <div
+              className={`${message.role === 'user' ? styles.userMessage : styles.gptMessage} ${
+                darkMode ? (message.role === 'user' ? darkStyles.userMessage : darkStyles.gptMessage) : ''
+              }`}
+            >
               {message.content}
             </div>
           </div>
         ))}
       </div>
-      <form className={styles.inputForm} onSubmit={handleSubmit}>
+      <form
+        className={`${styles.inputForm} ${darkMode ? darkStyles.inputForm : ''}`}
+        onSubmit={handleSubmit}
+      >
         <input
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
           placeholder="Type your message here..."
+          style={darkMode ? { backgroundColor: darkStyles.inputForm.inputBg, color: darkStyles.inputForm.input } : {}}
         />
         <button type="submit">Send</button>
       </form>
